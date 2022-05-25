75ca6c93a7b1f966342d9ce61a85afaeb90951b8
Carlos P <carlospuigcuadraemail@gmail.com>
2022-05-25 12:01:33 +0200

    Actualizar doc, Utiles.java y readme.md

--- .Utiles.java
+++ .Utiles.java
@@ -22,4 +22,22 @@ public class Utiles {
 		}
 	}
 	
+	/**
+	 * This function prints <code>prompt</code> to stdout and receives a <code>double</code> from stdin
+	 * It will keep asking until it receives a properly formatted <code>double</code>.
+	 * @param prompt Message to output to stdout before reading from stdin.
+	 * @param scanner The {@link java.util.Scanner} to use to read from stdin
+	 * @return The <code>double</code> received from stdin
+	 */
+	public static double askDouble(String prompt, Scanner scanner) {
+		while (true) {
+			try {
+				System.out.print(prompt);
+				return Double.valueOf(scanner.nextLine());
+			} catch (Exception e) {
+				System.out.println("Error de formato.");
+			}
+		}
+	}
+	
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# [Utiles.java](Utiles.java)
+Esta clase contiene dos funciones para recibir números distintos.
+La documentación se encuentra en [index.html](index.html)
